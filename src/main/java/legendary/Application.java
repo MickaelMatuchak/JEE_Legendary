4784ea1bc256650a3e742554002d4a68427af83f
Bermont Benjamin <bermont@b11p12.fil.univ-lille1.fr>
2017-12-18 13:39:44 +0100

    Ajout couleur cadre en fonction rareté item

--- src/main/java/legendary/Application.java
+++ src/main/java/legendary/Application.java
@@ -30,21 +30,21 @@ public class Application {
 
             // ARME
             itemRepository.save(new Item("Sabrolaser rouge", "Mythique sabrolaser de méchant", "arme", "epique", 20, (float) 20, 20, "sabrolaser-rouge.jpg"));
-            itemRepository.save(new Item("Sabrolaser vert", "Peu commun mais banal", "Arme", "Commun", 20, (float) 20, 20, "sabrolaser-vert.jpg"));            
+            itemRepository.save(new Item("Sabrolaser vert", "Peu commun mais banal", "Arme", "commun", 20, (float) 20, 20, "sabrolaser-vert.jpg"));            
             itemRepository.save(new Item("Sabrolaser bleu", "Il a appartenu a Obiwan Kenobi", "Arme", "epique", 20, (float) 20, 20, "sabrolaser-bleu.jpg"));
-            itemRepository.save(new Item("Double sabrolaser rouge", "La légende raconte qu'il a appartenu à Dark Maul", "Arme", "Légendaire", 30, (float) 20, 20, "sabrolaser-rouge-double.jpg"));
+            itemRepository.save(new Item("Double sabrolaser rouge", "La légende raconte qu'il a appartenu à Dark Maul", "Arme", "legendaire", 30, (float) 20, 20, "sabrolaser-rouge-double.jpg"));
             
             // CASQUE
             itemRepository.save(new Item("Casque du StormTrooper", "Casque d'un guerrier de l'empire", "casque", "commun", 1, (float) 20, 20, "casque-stormtrooper.jpg"));
-            itemRepository.save(new Item("Casque de Dark Vador", "Peut provoquer des difficultés de respiration", "casque", "légendaire", 30, (float) 5, 20, "casque-darkvador.jpg"));
-            itemRepository.save(new Item("Casque pilote B-Wing", "C'est le must pour un pilote", "casque", "commun", 1, (float) 20, 20, "casque-bwing.png"));
+            itemRepository.save(new Item("Casque de Dark Vador", "Peut provoquer des difficultés de respiration", "casque", "legendaire", 30, (float) 5, 20, "casque-darkvador.jpg"));
+            itemRepository.save(new Item("Casque pilote B-Wing", "C'est le must pour un pilote", "casque", "commun", 1, (float) 20, 20, "casque-bwing.jpg"));
             itemRepository.save(new Item("Casque de Chewbacca", "Il fait fureur pour le carnaval et effraie les ennemis", "casque", "commun", 1, (float) 20, 20, "casque-chewbacca.jpg"));
             
             // COMBINAISON
             itemRepository.save(new Item("Combinaison du StormTrooper", "Il parait que le blanc amincie", "combinaison", "commun", 1, (float) 20, 20, "combinaison-stormtrooper.jpg"));
             itemRepository.save(new Item("Combinaison de Chewbacca", "Parfait pour l'hiver. Laver à 30 degré.", "combinaison", "commun", 1, (float) 20, 20, "combinaison-chewbacca.jpg"));
-            itemRepository.save(new Item("Combinaison de Dark Vador", "Accoutrement légendaire", "combinaison", "commun", 30, (float) 20, 20, "combinaison-darkvador.jpeg"));
-            itemRepository.save(new Item("Combinaison pilote B-Wing", "Coupe très aérodynamique pour un pilotage interstéllaire", "combinaison", "commun", 1, (float) 20, 20, "combinaison-bwing.png"));
+            itemRepository.save(new Item("Combinaison de Dark Vador", "Accoutrement légendaire", "combinaison", "legendaire", 30, (float) 20, 20, "combinaison-darkvador.jpeg"));
+            itemRepository.save(new Item("Combinaison pilote B-Wing", "Coupe très aérodynamique pour un pilotage interstéllaire", "combinaison", "rare", 1, (float) 20, 20, "combinaison-bwing.png"));
         };
     }
 }
